b1ad14c30e3d7ddeae7f40a088fc2fcbf85be3ad
jimao <1982775886@qq.com>
2020-10-29 15:29:15 +0800

    思考如何处理JSON

--- src/jsField.go
+++ src/jsField.go
@@ -5,3 +5,18 @@ type JsField struct {
 	name  string
 	value JsValue
 }
+
+// Name 获取Js字段名称
+func (me *JsField) Name() string {
+	return me.name
+}
+
+// Value 获取Js字段值
+func (me *JsField) Value() JsValue {
+	return me.value
+}
+
+// Type 获取Js字段类型
+func (me *JsField) Type() EJsType {
+	return me.value.Type()
+}
--- src/jsValue.go
+++ src/jsValue.go
@@ -6,3 +6,27 @@ type JsValue struct {
 	objectFields []JsField
 	arrayValues  []JsValue
 }
+
+// Type 获取Js值的类型
+func (me *JsValue) Type() EJsType {
+	return me.jsType
+}
+
+// ObjectFields 获取JsObject类型的字段列表
+func (me *JsValue) ObjectFields() []JsField {
+	return me.objectFields
+}
+
+// ArrayValues 获取JsArray类型的值列表
+func (me *JsValue) ArrayValues() []JsValue {
+	return me.arrayValues
+}
+
+// NewJsValue 构造函数
+func NewJsValue(value interface{}) *JsValue {
+	return &JsValue{
+		jsType:       JsUndefined,
+		objectFields: []JsField{},
+		arrayValues:  []JsValue{},
+	}
+}
