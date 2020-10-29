dac859e168f47b76dece1ded608953cb67f5c025
jimao <1982775886@qq.com>
2020-10-29 18:48:20 +0800

    提交代码

--- src/main.go
+++ src/main.go
@@ -10,10 +10,16 @@ func main() {
 	fmt.Println("程序开始")
 	stu := Student{"鸡毛巾", true, 13}
 	stu.ShowMetaInfos()
-	bytes, err := json.Marshal(true)
+	bytes, err := json.Marshal(stu)
 	if err != nil {
 		log.Fatalln("JSON转换失败")
 	}
 	jsonText := string(bytes)
 	fmt.Println(jsonText)
+	var stu2 Student
+	err = json.Unmarshal([]byte(jsonText), &stu2)
+	if err != nil {
+		log.Fatalln("JSON反序列化失败")
+	}
+	stu2.ShowMetaInfos()
 }
