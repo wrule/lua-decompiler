1f8e76cd73d63c80127dc54b83e116d65bc9bdde
jimao <1982775886@qq.com>
2020-10-29 15:57:31 +0800

    JSON实践

--- src/main.go
+++ src/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 )
 
 func main() {
 	fmt.Println("程序开始")
 	stu := Student{"鸡毛巾", true, 13}
 	stu.ShowMetaInfos()
+	bytes, err := json.Marshal(stu)
+	if err != nil {
+		log.Fatalln("JSON转换失败")
+	}
+	jsonText := string(bytes)
+	fmt.Println(jsonText)
 }
