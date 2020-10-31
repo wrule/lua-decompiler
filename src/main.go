93cbe14d05594d97d1d73861d6247138ebab1eb9
jimao <1982775886@qq.com>
2020-10-31 15:39:14 +0800

    奇怪的问题

--- src/main.go
+++ src/main.go
@@ -1,26 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 
 	"./js"
+	"./utils"
 )
 
 func main() {
-	var jsonObj map[string]interface{}
-	err := json.Unmarshal([]byte(`{"data":`+`
+	var jsonText = `
 {
 	"name": "jimao",
 	"sex": true,
 	"tags": ["3", "2", "1", 1]
-}
-`+`}`), &jsonObj)
-	if err != nil {
-		log.Fatalln("JSON反序列化失败")
-		return
-	}
+}	
+`
+
+	var jsonObj = utils.WrapParse(jsonText)
 
 	var value = js.NewJsValue(jsonObj)
 
--- src/utils/json.go
+++ src/utils/json.go
@@ -1,6 +1,39 @@
-package json
+package utils
+
+import (
+	"encoding/json"
+)
+
+// Stringify JSON序列化
+func Stringify(value interface{}) string {
+	bytes, err := json.Marshal(value)
+	if err != nil {
+		panic("JSON序列化错误")
+	}
+	return string(bytes)
+}
+
+// Parse JSON反序列化
+func Parse(jsonText string) map[string]interface{} {
+	var jsonObj map[string]interface{}
+	err := json.Unmarshal([]byte(jsonText), &jsonObj)
+	if err != nil {
+		panic("JSON反序列化错误")
+	}
+	return jsonObj
+}
 
 // Wrap 在JSON外层包裹一个对象结构
-func Wrap(json string) string {
-	return `{"data":` + json + "}"
+func Wrap(jsonText string) string {
+	return `{"data":` + jsonText + "}"
+}
+
+// WrapStringify 自带拆包的JSON序列化
+func WrapStringify(value interface{}) string {
+	return Stringify(value.(map[string]interface{})["data"])
+}
+
+// WrapParse 自带包裹的JSON反序列化
+func WrapParse(jsonText string) map[string]interface{} {
+	return Parse(Wrap(jsonText))
 }
