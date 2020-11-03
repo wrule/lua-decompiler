da9fe5b146fa511177b8d78bab79fc291aee4d3b
jimao <1982775886@qq.com>
2020-11-03 17:17:50 +0800

    十六进制输出

--- src/main.go
+++ src/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	fmt.Println("你好Lua")
+	file, err := os.OpenFile("./test/1.out", os.O_RDONLY, os.ModeType)
+	if err != nil {
+		log.Fatalln("打开文件失败")
+		return
+	}
+	info, err := file.Stat()
+	if err != nil {
+		return
+	}
+	fmt.Println(info.Size())
+	bytes := make([]byte, 10)
+	size, err := file.Read(bytes)
+	if err != nil {
+		return
+	}
+	fmt.Println(size)
+	for index, item := range bytes {
+		fmt.Printf("%d %02X %s\n", index, item, string(item))
+	}
+	file.Close()
 }
