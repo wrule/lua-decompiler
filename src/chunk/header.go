9c4bcc23d2327fd586e81fda70002078fe05a10e
jimao <1982775886@qq.com>
2020-11-04 12:09:39 +0800

    header部分的checkload基本上完成

--- src/chunk/header.go
+++ src/chunk/header.go
@@ -14,3 +14,142 @@ type Header struct {
 	luacInt         int64
 	luacNum         float64
 }
+
+const (
+	// LuaSignature 魔数
+	LuaSignature = "\x1bLua"
+	// LuacVersion Luac版本
+	LuacVersion = 0x53
+	// LuacFormat Luac格式数字
+	LuacFormat = 0x00
+	// LuacData Luac预留验证数据
+	LuacData = "\x19\x93\r\n\x1a\n"
+	// CIntSize Lua虚拟机cint宽度
+	CIntSize = 4
+	// SizeTSize Lua虚拟机size_t宽度
+	SizeTSize = 8
+	// InstructionSize Lua虚拟机指令宽度
+	InstructionSize = 4
+	// LuaIntegerSize Lua虚拟机整数宽度
+	LuaIntegerSize = 8
+	// LuaNumberSize Lua虚拟机浮点数宽度
+	LuaNumberSize = 8
+	// LuacInt 整数验证数字
+	LuacInt = 0x5678
+	// LuacNum 浮点数验证数字
+	LuacNum = 370.5
+)
+
+// CheckLoad 从Reader中检查并加载Header
+func (me *Header) CheckLoad(reader *Reader) {
+	{
+		res, err := reader.ReadBytes(4)
+		if err != nil {
+			panic("读取Lua签名失败")
+		}
+		if string(res) != LuaSignature {
+			panic("文件没有有效的Lua签名")
+		}
+		copy(me.signature[:], res)
+	}
+	{
+		res, err := reader.ReadByte()
+		if err != nil {
+			panic("读取Luac版本失败")
+		}
+		if res != LuacVersion {
+			panic("Luac版本校验失败")
+		}
+		me.version = res
+	}
+	{
+		res, err := reader.ReadByte()
+		if err != nil {
+			panic("读取Luac格式数字失败")
+		}
+		if res != LuacFormat {
+			panic("Luac格式数字校验失败")
+		}
+		me.format = res
+	}
+	{
+		res, err := reader.ReadBytes(6)
+		if err != nil {
+			panic("读取Luac验证数据失败")
+		}
+		if string(res) != LuacData {
+			panic("文件没有有效的Luac验证数据")
+		}
+		copy(me.luacData[:], res)
+	}
+	{
+		res, err := reader.ReadByte()
+		if err != nil {
+			panic("读取cint宽度失败")
+		}
+		if res != CIntSize {
+			panic("cint宽度校验失败")
+		}
+		me.cintSize = res
+	}
+	{
+		res, err := reader.ReadByte()
+		if err != nil {
+			panic("读取sizetSize宽度失败")
+		}
+		if res != SizeTSize {
+			panic("sizetSize宽度校验失败")
+		}
+		me.sizetSize = res
+	}
+	{
+		res, err := reader.ReadByte()
+		if err != nil {
+			panic("读取指令宽度失败")
+		}
+		if res != InstructionSize {
+			panic("指令宽度校验失败")
+		}
+		me.instructionSize = res
+	}
+	{
+		res, err := reader.ReadByte()
+		if err != nil {
+			panic("读取LuaInteger宽度失败")
+		}
+		if res != LuaIntegerSize {
+			panic("LuaInteger宽度校验失败")
+		}
+		me.luaIntegerSize = res
+	}
+	{
+		res, err := reader.ReadByte()
+		if err != nil {
+			panic("读取LuaNumber宽度失败")
+		}
+		if res != LuaNumberSize {
+			panic("LuaNumber宽度校验失败")
+		}
+		me.luaNumberSize = res
+	}
+	{
+		res, err := reader.ReadLuaInteger()
+		if err != nil {
+			panic("读取Luac整数验证数字失败")
+		}
+		if res != LuacInt {
+			panic("Luac整数验证数字校验失败")
+		}
+		me.luacInt = res
+	}
+	{
+		res, err := reader.ReadLuaNumber()
+		if err != nil {
+			panic("读取Luac浮点数验证数字失败")
+		}
+		if res != LuacNum {
+			panic("Luac浮点数验证数字校验失败")
+		}
+		me.luacNum = res
+	}
+}
--- src/main.go
+++ src/main.go
@@ -4,28 +4,31 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"./chunk"
 )
 
 func main() {
-	// fmt.Println("你好Lua")
 	file, err := os.OpenFile("./test/1.out", os.O_RDONLY, os.ModeType)
 	if err != nil {
 		log.Fatalln("打开文件失败")
 		return
 	}
-	info, err := file.Stat()
+
+	var reader = chunk.NewReader(file)
+
+	var header chunk.Header
+
+	header.CheckLoad(reader)
+
+	res, err := reader.ReadUint32()
 	if err != nil {
 		return
 	}
-	fmt.Println("文件大小: ", info.Size())
-	bytes := make([]byte, 5)
-	size, err := file.Read(bytes)
-	if err != nil {
-		return
-	}
-	fmt.Println("读取大小: ", size)
-	for index, item := range bytes {
-		fmt.Printf("%d %02X\n", index, item)
-	}
+	fmt.Println(res)
+
+	// 1B 4C 75 61
+	// 27 76 117 97
+
 	file.Close()
 }
