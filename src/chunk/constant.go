185d662c7664cd5424ea45ee05e5916f04cdbc20
jimao <1982775886@qq.com>
2020-11-05 16:43:16 +0800

    常量tag注释

--- src/chunk/constant.go
+++ src/chunk/constant.go
@@ -1,10 +1,16 @@
 package chunk
 
 const (
-	TagNil      = 0x00
-	TagBoolean  = 0x01
-	TagNumber   = 0x03
-	TagInteger  = 0x13
+	// TagNil nil常量类型
+	TagNil = 0x00
+	// TagBoolean 布尔值常量类型
+	TagBoolean = 0x01
+	// TagNumber 浮点数常量类型
+	TagNumber = 0x03
+	// TagInteger 整数常量类型
+	TagInteger = 0x13
+	// TagShortStr 短字符串常量类型
 	TagShortStr = 0x04
-	TagLongStr  = 0x14
+	// TagLongStr 长字符串常量类型
+	TagLongStr = 0x14
 )
