04c7d50203dd37e1f15967fefb3b26852887d2a6
jimao <1982775886@qq.com>
2020-11-05 23:25:24 +0800

    添加注释

--- src/chunk/prototype.go
+++ src/chunk/prototype.go
@@ -17,55 +17,67 @@ type Prototype struct {
 	upvalueNames    []string
 }
 
-// Source 代码文件位置
+// Source 获取源文件名
 func (me *Prototype) Source() string {
 	return me.source
 }
 
+// LineDefined 获取开始行号
 func (me *Prototype) LineDefined() uint32 {
 	return me.lineDefined
 }
 
+// LastLineDefined 获取结束行号
 func (me *Prototype) LastLineDefined() uint32 {
 	return me.lastLineDefined
 }
 
+// NumParams 获取固定参数个数
 func (me *Prototype) NumParams() byte {
 	return me.numParams
 }
 
+// IsVararg 是否是Vararg函数
 func (me *Prototype) IsVararg() byte {
 	return me.isVararg
 }
 
+// MaxStackSize 获取需要寄存器数量
 func (me *Prototype) MaxStackSize() byte {
 	return me.maxStackSize
 }
 
+// Codes 获取指令表
 func (me *Prototype) Codes() []uint32 {
 	return me.codes
 }
 
+// Constants 获取常量表
 func (me *Prototype) Constants() []interface{} {
 	return me.constants
 }
 
+// Upvalues 获取Upvalue表
 func (me *Prototype) Upvalues() []Upvalue {
 	return me.upvalues
 }
 
+// Prototypes 获取子函数原型表
 func (me *Prototype) Prototypes() []*Prototype {
 	return me.protos
 }
 
+// LineInfos 获取行号表
 func (me *Prototype) LineInfos() []uint32 {
 	return me.lineInfos
 }
 
+// LocVars 获取局部变量表
 func (me *Prototype) LocVars() []LocVar {
 	return me.locVars
 }
 
+// UpvalueNames 获取Upvalue名列表
 func (me *Prototype) UpvalueNames() []string {
 	return me.upvalueNames
 }
