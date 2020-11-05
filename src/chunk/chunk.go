08d36c30e7de89e555f7425a82cd5099b9a57496
jimao <1982775886@qq.com>
2020-11-05 17:22:02 +0800

    chunk的checkload

--- src/chunk/chunk.go
+++ src/chunk/chunk.go
@@ -6,3 +6,10 @@ type Chunk struct {
 	sizeUpdatevalues byte
 	mainFunc         *Prototype
 }
+
+// CheckLoad 校验并加载Chunk
+func (me *Chunk) CheckLoad(reader *Reader) {
+	me.header.CheckLoad(reader)
+	me.sizeUpdatevalues = reader.ReadByte()
+	me.mainFunc = reader.ReadPrototype("")
+}
