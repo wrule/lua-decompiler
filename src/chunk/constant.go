6efef792f28a5d68744b0b3f0af156ac1d7c1f93
jimao <1982775886@qq.com>
2020-11-06 13:42:30 +0800

    重构常量

--- src/chunk/constant.go
+++ src/chunk/constant.go
@@ -1,19 +1,40 @@
 package chunk
 
 // Constant 常量空接口
-type Constant interface{}
+type Constant struct {
+	ctype EConstantType
+	value interface{}
+}
+
+// Type 常量的类型
+func (me *Constant) Type() EConstantType {
+	return me.ctype
+}
+
+// Value 常量的值
+func (me *Constant) Value() interface{} {
+	return me.value
+}
+
+// ValueString 常量的值用字符串形式表达
+func (me *Constant) ValueString() string {
+	return ""
+}
+
+// EConstantType Lua常量类型枚举
+type EConstantType byte
 
 const (
-	// TagNil nil常量类型
-	TagNil = 0x00
-	// TagBoolean 布尔值常量类型
-	TagBoolean = 0x01
-	// TagNumber 浮点数常量类型
-	TagNumber = 0x03
-	// TagInteger 整数常量类型
-	TagInteger = 0x13
-	// TagShortStr 短字符串常量类型
-	TagShortStr = 0x04
-	// TagLongStr 长字符串常量类型
-	TagLongStr = 0x14
+	// ConstantTypeNil nil常量类型
+	ConstantTypeNil EConstantType = 0x00
+	// ConstantTypeBoolean 布尔值常量类型
+	ConstantTypeBoolean EConstantType = 0x01
+	// ConstantTypeNumber 浮点数常量类型
+	ConstantTypeNumber EConstantType = 0x03
+	// ConstantTypeInteger 整数常量类型
+	ConstantTypeInteger EConstantType = 0x13
+	// ConstantTypeShortStr 短字符串常量类型
+	ConstantTypeShortStr EConstantType = 0x04
+	// ConstantTypeLongStr 长字符串常量类型
+	ConstantTypeLongStr EConstantType = 0x14
 )
