78e263767b47e7e82c1047b1f99519ede43cf98c
jimao <1982775886@qq.com>
2020-11-11 14:15:49 +0800

    parser字符串转化为数字

--- src/number/parser.go
+++ src/number/parser.go
@@ -1,11 +1,17 @@
 package number
 
+import (
+	"strconv"
+)
+
 // ParseInteger 字符串转换成为int64
 func ParseInteger(str string) (int64, bool) {
-	return 0, true
+	num, err := strconv.ParseInt(str, 10, 64)
+	return num, err == nil
 }
 
 // ParseFloat 字符串转换成为float64
 func ParseFloat(str string) (float64, bool) {
-	return 0, true
+	num, err := strconv.ParseFloat(str, 64)
+	return num, err == nil
 }
