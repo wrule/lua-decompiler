6fcabe5f54137dfb4b50b73dbed1c9a8191ef941
jimao <1982775886@qq.com>
2020-11-13 00:40:22 +0800

    这一章代码完成

--- src/api/misc.go
+++ src/api/misc.go
@@ -8,3 +8,23 @@ func (me *LuaState) Len(index int) int {
 	}
 	panic("长度计算错误")
 }
+
+// Concat 没有按照书上做
+func (me *LuaState) Concat(n int) {
+	if n == 0 {
+		me.PushString("")
+	} else if n >= 2 {
+		var result = ""
+		for i := 0; i < n; i++ {
+			if me.IsString(-1) {
+				value := me.stack.Pop()
+				str, _ := value.ToStringX()
+				result += str
+			} else {
+				panic("Concat连接错误")
+			}
+		}
+		me.PushString(result)
+	}
+	// 为1的话什么都不做
+}
