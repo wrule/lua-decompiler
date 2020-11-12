0719f66286da1a5f03d1f62b69000404428207b4
jimao <1982775886@qq.com>
2020-11-13 00:12:17 +0800

    Len方法

--- src/api/misc.go
+++ src/api/misc.go
@@ -1,1 +1,10 @@
 package api
+
+// Len 获取栈中值的长度
+func (me *LuaState) Len(index int) int {
+	value := me.stack.Get(index)
+	if str, ok := value.Value().(string); ok {
+		me.PushInteger(int64(len(str)))
+	}
+	panic("长度计算错误")
+}
